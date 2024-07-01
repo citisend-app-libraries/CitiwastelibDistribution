ce7ae351dad3b63e9479186de8802064021890c1
Alberto Bailac <alberto.bailac@m.e-tecnia.es>
2024-07-01 19:36:45 +0200

    New Citiwastelibs version 0.0.5

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
-let version = "0.0.4"
+let version = "0.0.5"
 let moduleName = "Citiwastelibs"
 let checksum = ""
 
