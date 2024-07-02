e8709ce5ccd618455479a96d1f8fb5c8b1eed09a
Alberto Bailac <alberto.bailac@m.e-tecnia.es>
2024-07-02 09:08:16 +0200

    New Citiwastelibs version 0.0.10

--- Package.swift
+++ Package.swift
@@ -2,17 +2,20 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
-let version = "0.0.9"
+let version = "0.0.10"
 let moduleName = "Citiwastelibs"
 let checksum = "5dc7abe3ad4d3ffefae3c9dc87ca38a2adc7f65b604f929b5761b7c7b206465b"
 
 let package = Package(
     name: moduleName,
+    platforms: [
+       .iOS(.v13) //, .macCatalyst(.v15), .macOS(.v10_15)
+    ],
     products: [
-        .library(
-            name: moduleName,
-            targets: [moduleName]
-        )
+       .library(
+           name: moduleName,
+           targets: [moduleName]
+       )
     ],
     targets: [
            .binaryTarget(
