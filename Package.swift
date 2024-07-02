89e1fafd75095b3fff241955025e2845ae2df312
AlbertoEtecnia <alberto.bailac@m.e-tecnia.es>
2024-07-02 09:34:53 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,15 +2,9 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
-<<<<<<< HEAD
 let version = "0.0.10"
 let moduleName = "Citiwastelib"
 let checksum = "d917eda4fd76e80f29cd4a86ae773c297bb9b443c2af1fdbb0e8385ac7fe4241"
-=======
-let version = "0.0.10"
-let moduleName = "Citiwastelib"
-let checksum = "d917eda4fd76e80f29cd4a86ae773c297bb9b443c2af1fdbb0e8385ac7fe4241"
->>>>>>> e8709ce5ccd618455479a96d1f8fb5c8b1eed09a
 
 let package = Package(
     name: moduleName,
