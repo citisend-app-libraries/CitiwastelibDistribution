9e25c1a59573d52d2c6b3f4fac63d25fc8479e84
Alberto Bailac <alberto.bailac@m.e-tecnia.es>
2024-07-02 09:17:51 +0200

    no message

--- Package.swift
+++ Package.swift
@@ -2,9 +2,9 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
-let version = "0.0.2"
+let version = "0.0.3"
 let moduleName = "Citiwastelib"
-let checksum = "edcf14ce6a48d1c0be6b09ad8e5a3bb027f645d61564e1b481d6e2f141c81c6a"
+let checksum = "b567383eea1e35c0fec2f1916981ef1557017da65f096537e8af90af0da54f78"
 
 let package = Package(
     name: moduleName,
