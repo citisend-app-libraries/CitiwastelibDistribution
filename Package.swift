628ce66f75303f42970af11e006dd14e5437da7b
Alberto Bailac <alberto.bailac@m.e-tecnia.es>
2024-07-02 09:20:26 +0200

    New Citiwastelib version 0.0.10

--- Package.swift
+++ Package.swift
@@ -3,13 +3,13 @@
 import PackageDescription
 
 <<<<<<< HEAD
-let version = "0.0.3"
+let version = "0.0.10"
 let moduleName = "Citiwastelib"
-let checksum = "b567383eea1e35c0fec2f1916981ef1557017da65f096537e8af90af0da54f78"
+let checksum = "d917eda4fd76e80f29cd4a86ae773c297bb9b443c2af1fdbb0e8385ac7fe4241"
 =======
 let version = "0.0.10"
-let moduleName = "Citiwastelibs"
-let checksum = "5dc7abe3ad4d3ffefae3c9dc87ca38a2adc7f65b604f929b5761b7c7b206465b"
+let moduleName = "Citiwastelib"
+let checksum = "d917eda4fd76e80f29cd4a86ae773c297bb9b443c2af1fdbb0e8385ac7fe4241"
 >>>>>>> e8709ce5ccd618455479a96d1f8fb5c8b1eed09a
 
 let package = Package(
