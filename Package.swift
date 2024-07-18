534f34e11091dc57e65659c50cbc202f8d5eac08
Alberto Bailac <alberto.bailac@m.e-tecnia.es>
2024-07-18 13:59:32 +0200

    New Citiwastelib version 0.0.16

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let version = "0.0.16"
 let moduleName = "Citiwastelib"
-let checksum = "9eaec37a1d50a6728b25a8237714a10e448b446e224d66d8d8bd6095c32213f5"
+let checksum = "a1043ade2b1f16048e727fcdf58b2b8b1e0448fdd1ba03a3c822acc4f4878296"
 
 let package = Package(
     name: moduleName,
